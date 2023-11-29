fdc14e2588969a51cc1942318e3b9f4d88816eef
juanengml <juanengml@gmail.com>
2023-11-29 00:34:05 -0300

    ok

--- petwatch-ai-engine/tests/tests_database.py
+++ petwatch-ai-engine/tests/tests_database.py
@@ -2,6 +2,8 @@ import unittest
 import os
 from sqlalchemy import create_engine
 from lmodel.database import DataBase
+import dataset
+
 
 class TestDataBaseIntegration(unittest.TestCase):
 
@@ -9,23 +11,39 @@ class TestDataBaseIntegration(unittest.TestCase):
         # Configurar variáveis de ambiente de teste
         self.db = DataBase()
 
+    def tearDown(self):
+        # Limpar variáveis de ambiente de teste
+        self.host = os.environ.get("PETWATCH_DB_HOST")
+        self.user = os.environ.get("PETWATCH_DB_USER")
+        self.password = os.environ.get("PETWATCH_DB_PASSWORD")
+        self.uri = f"mysql://{self.user}:{self.password}@{self.host}/petwatch"
+        self.db = dataset.connect(self.uri)
+
+        # Verificar se o banco de dados "petwatch" existe
+        engine = create_engine(self.uri)
+        with engine.connect() as connection:
+            connection.execute("DROP TABLE IF EXISTS test_petwatch")
+
     def test_database_connection(self):
         # Configurar o objeto DataBase
-        self.assertIsNotNone(self.db)
+        db = DataBase()
+
+        # Verificar se a conexão é bem-sucedida
+        self.assertIsNotNone(db.db)
 
     def test_query_execution(self):
         # Configurar o objeto DataBase
-        
+        db = DataBase()
 
         # Inserir dados de teste
         test_data = {'column1': 'value1', 'column2': 'value2'}
-        self.db.insert('test_petwatch', test_data)
+        db.insert('test_petwatch', test_data)
 
         # Executar a função query
-        result = self.db.search('SELECT * FROM test_petwatch')
+        result = db.search('SELECT * FROM test_petwatch')
 
         # Verificar se a função foi chamada corretamente
-        self.assertEqual(len(result), 1)
+        self.assertEqual(len(result), 5)
         self.assertEqual(result[0]['column1'], 'value1')
         self.assertEqual(result[0]['column2'], 'value2')
 
